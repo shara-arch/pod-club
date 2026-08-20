81bb6141c3051e6519ab4c214d49c99a1aafadd0
Sharon Moegi <sharamoegi@gmail.com>
2026-08-20 20:20:36 +0300

    Import dependencies for Protected Route

---
+++ src/routes/Protected.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from './AuthContext';
+
+export default function ProtectedRoute({ children }) {
+ return (
+    
+ )
+}

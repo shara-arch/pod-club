c66f6f8100f06eb947ddf342d395b73960141254
Sharon Moegi <sharamoegi@gmail.com>
2026-08-20 16:27:20 +0300

    Store currently authenticated user state

--- src/routes/AuthContext.jsx
+++ src/routes/AuthContext.jsx
@@ -2,3 +2,9 @@ import React, {createContext, useContext, useState, useEffect} from 'react';
 
 // Create a Context object to hold and share authentication state across components
 const AuthContext = createContext(null);
+export default function AuthProvider( {children}) {
+    // Store currently authenticated user state(load from localStorage on initial render)
+  const [currentUser, setCurrentUser] = useState(() => {
+    return JSON.parse(localStorage.getItem('activeUser')) || null;
+  });
+}

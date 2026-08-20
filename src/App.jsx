574f0e284e07ff5416597c0fa229989c54b146db
Sharon Moegi <sharamoegi@gmail.com>
2026-08-20 23:22:10 +0300

    Add Routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './components/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
+import AuthProvider from './routes/AuthContext';
 import Login from './components/Login';
+import ProtectedRoute from './routes/Protected';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+               <p>WElcome to pod-club</p>
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;

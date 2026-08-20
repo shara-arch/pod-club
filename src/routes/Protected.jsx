313c103049dc667912864c477374f32cf978d758
Sharon Moegi <sharamoegi@gmail.com>
2026-08-20 20:21:22 +0300

    Add ProtectedRoute component to redirect unauthenticated users

--- src/routes/Protected.jsx
+++ src/routes/Protected.jsx
@@ -3,7 +3,13 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
 export default function ProtectedRoute({ children }) {
- return (
-    
- )
+  const { currentUser } = useAuth();
+  const location = useLocation();
+
+  if (!currentUser) {
+    // Redirect unauthenticated user to /login and save current location
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
 }
